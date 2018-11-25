8467cec5079587cfeb03980ddc4bd7beb23caa93
LONG <hoanglong240797@gmail.com>
2018-11-25 20:36:44 +0700

    dump dashboard layout

--- loldex/src/components/dashboard/DashboardBody.vue
+++ loldex/src/components/dashboard/DashboardBody.vue
@@ -1,17 +1,77 @@
 <template>
     <div class="dashboard-body">
         <div class="row dashboard-body__row">
+            <!-- col 1 -->
             <div class="col-md-3 dashboard-body__col">
-                <body-panel :title="'Balance'">
-                    <b-tabs nav-wrapper-class="tab-header">
-                        <b-tab title="one" active>
-                            <h1>ONE HEHE</h1>
-                        </b-tab>
-                        <b-tab title="two">
-                            <h1>TWO HEHE</h1>
-                        </b-tab>
-                    </b-tabs>
-                </body-panel>
+                <div class="dashboard-body__item">
+                    <body-panel :title="'Balance'">
+                        <b-tabs nav-wrapper-class="tab-header">
+                            <b-tab title="Deposit" active>
+                            </b-tab>
+                            <b-tab title="Withdraw">
+                            </b-tab>
+                            <b-tab title="Transfer">
+                            </b-tab>
+                        </b-tabs>
+                    </body-panel>
+                </div>
+                <div class="dashboard-body__item">
+                    <body-panel :title="'Volume'">
+                    
+                    </body-panel>
+                </div>
+            </div>
+            <!-- col 2 -->
+            <div class="col-md-2 dashboard-body__col">
+                <div class="dashboard-body__item">
+                    <body-panel :title="'Order Book'">
+                        
+                    </body-panel>
+                </div>
+                <div class="dashboard-body__item">
+                    <body-panel :title="'New Order'">
+                        <b-tabs nav-wrapper-class="tab-header">
+                            <b-tab title="Buy" active>
+                            </b-tab>
+                            <b-tab title="Sell">
+                            </b-tab>
+                        </b-tabs>
+                    </body-panel>
+                </div>
+            </div>
+            <!-- col 3 -->
+            <div class="col-md-4 dashboard-body__col">
+                <div class="dashboard-body__item">
+                    <body-panel :title="'Order Book'">
+                        <b-tabs nav-wrapper-class="tab-header">
+                            <b-tab title="Price" active>
+                            </b-tab>
+                            <b-tab title="Depth">
+                            </b-tab>
+                        </b-tabs>
+                    </body-panel>
+                </div>
+                <div class="dashboard-body__item">
+                    <body-panel :title="'New Order'">
+                        <b-tabs nav-wrapper-class="tab-header">
+                            <b-tab title="Important" active>
+                            </b-tab>
+                            <b-tab title="Trades">
+                            </b-tab>
+                            <b-tab title="Orders">
+                            </b-tab>
+                            <b-tab title="Funds">
+                            </b-tab>
+                        </b-tabs>
+                    </body-panel>
+                </div>
+            </div>
+            <!-- col 4 -->
+            <div class="col-md-3 dashboard-body__col">
+                <div class="dashboard-body__item">
+                    <body-panel :title="'Trades'">
+                    </body-panel>
+                </div>
             </div>
         </div>
     </div>
@@ -50,7 +110,16 @@
             height: 100%;            
         }
         .dashboard-body__col {
-            height: 100%;            
+            height: 100%; 
+            border-right: 2px solid #18252e;     
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;  
+            flex-basis: 50%;  
+
+            .dashboard-body__item {
+                flex-grow: 1;
+            }
         }
     }
 </style>
